eccb594d9934b253b38a30b60bc772ce8f0b3408
Andrew Reddin <areddin409@gmail.com>
2025-07-08 16:48:41 -0500

    feat: integrate Clerk for authentication and update app layout
    - Added @clerk/clerk-expo and @expo/vector-icons dependencies.
    - Refactored main layout to use ClerkProvider and Slot for routing.
    - Removed unused history and profile pages.
    - Created a new tab layout for navigation with screens for Home, Exercises, Workout, Active Workout, History, and Profile.
    - Implemented basic structure for each tab screen.
    - Added Prettier configuration for code formatting.
    - Updated VSCode settings for spell checking.

---
+++ src/app/(app)/(tabs)/_layout.tsx
@@ -0,0 +1,80 @@
+import AntDesign from '@expo/vector-icons/AntDesign';
+import { Tabs } from 'expo-router';
+import { Image } from 'react-native';
+
+const Layout = () => {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name='index'
+        options={{
+          headerShown: false,
+          title: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name='home' size={size} color={color} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name='exercises'
+        options={{
+          headerShown: false,
+          title: 'Exercises',
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name='book' size={size} color={color} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name='workout'
+        options={{
+          headerShown: false,
+          title: 'Workout',
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name='pluscircle' size={size} color={color} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name='active-workout'
+        options={{
+          headerShown: false,
+          title: 'Active Workout',
+          href: null,
+          tabBarStyle: { display: 'none' },
+        }}
+      />
+
+      <Tabs.Screen
+        name='history'
+        options={{
+          headerShown: false,
+          title: 'History',
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name='clockcircleo' size={size} color={color} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name='profile'
+        options={{
+          headerShown: false,
+          title: 'Profile',
+          // tabBarIcon: ({ color, size }) => (
+          //   // <Image
+          //   //   source={user?.imageUrl ?? user?.externalAccounts?.[0]?.imageUrl}
+          //   //   className='rounded-full'
+          //   //   style={{ width: 28, height: 28, borderRadius: 100 }}
+          //   // />
+          // ),
+        }}
+      />
+    </Tabs>
+  );
+};
+
+export default Layout;
